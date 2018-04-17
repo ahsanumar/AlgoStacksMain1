322bb84a63c0a3be91ae6f70a4b3c57d7284c79d
ahsanumar <32022750+ahsanumar@users.noreply.github.com>
2018-04-17 19:18:58 +0500

    Added testemonial

--- head.php
+++ head.php
@@ -7,7 +7,9 @@
 	<title><?php if($pageName){ echo $pageName; } ?> | Algo Stacks</title>
 	<link rel="shortcut icon" type="image/png" href="assets/images/favicon.png"/>
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/fontawesome.min.css" media="all">
 	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="assets/css/test.css">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -8,6 +8,8 @@ require_once "nav.php";
 
 require_once "mainJmb.php";
 
+require_once "testemonial.php";
+
 require_once "footer.php";
 
 ?>
