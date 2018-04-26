489979c4d499e1390e3278c09f5d6c6c67cd346c
ahsanumar <32022750+ahsanumar@users.noreply.github.com>
2018-04-26 23:14:49 +0500

    Fixed login form

--- loginModal.php
+++ loginModal.php
@@ -1,33 +1,35 @@
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-center" id="loginModalLabel">Login</h5>
-            </div>
-            <div class="modal-body">
-                    <div class="container">
-                            <form class="clearfix">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <label for="email" class="form-label e-mail">Email or Phone</label>
-                                    <input type="email" class="form-control inputSize" id="email" >
-                                </div>
-                                
-                                <div href="#" class="btn btn-info btn-sm" role="button">
-                                    <button type="button" class="btn btn-info">Login</button>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                    <label for="passwrd" class="form-label passwrd">Password</label>
-                                    <input type="text" class="form-control inputSize" id="xpasswrd" >
-                                </div>
-                            </form> 
-                        </div>
-                    </div>
-                </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            </div>
-        </div>
-    </div>
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title text-center" id="loginModalLabel">Login</h5>
+			</div>
+			<div class="modal-body">
+				<div class="container">
+					<form class="clearfix">
+						<div class="row">
+							<div class="col-sm-12 col-md-12 col-lg-12">
+								<label for="email" class="form-label e-mail">Email or Phone</label>
+								<input type="email" class="form-control inputSize" id="email">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-12 col-md-12 col-lg-12">
+								<label for="passwrd" class="form-label passwrd">Password</label>
+								<input type="text" class="form-control inputSize" id="xpasswrd">
+							</div>
+						</div>
+						<div class="row loginModalButtonMargin">
+							<div class="col-sm-12 col-md-12 col-lg-12">
+								<button type="button" class="btn btn-info">Login</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+			</div>
+		</div>
+	</div>
 </div>
