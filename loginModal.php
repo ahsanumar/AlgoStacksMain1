5ec9f8b1d7a9d8245047227497ffc36a56f0b596
Shahzad07618 <38255795+Shahzad07618@users.noreply.github.com>
2018-04-26 10:21:57 -0700

    added

--- loginModal.php
+++ loginModal.php
@@ -5,8 +5,25 @@
                 <h5 class="modal-title text-center" id="loginModalLabel">Login</h5>
             </div>
             <div class="modal-body">
-                Login form comes here!
-            </div>
+                    <div class="container">
+                            <form class="clearfix">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label for="email" class="form-label e-mail">Email or Phone</label>
+                                    <input type="email" class="form-control inputSize" id="email" >
+                                </div>
+                                <div class="col-md-3">
+                                    <label for="passwrd" class="form-label passwrd">Password</label>
+                                    <input type="text" class="form-control inputSize" id="xpasswrd" >
+                                </div>
+                                <div href="#" class="btn btn-info btn-sm" role="button">
+                                    <button type="button" class="btn btn-info">Login</button>
+                                </div>
+                            </div>
+                            </form> 
+                        </div>
+                    </div>
+                </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
             </div>
